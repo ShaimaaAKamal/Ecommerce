125a66a55a31f49c9b4eac87dc1abce283ddc3ae
Shaimaa Kamal <Shaimaakamal039@gmail.com>
2022-06-03 21:08:01 +0200

    Connection to database

---
+++ db/dbConnection.js
@@ -0,0 +1,22 @@
+const mongoose=require('mongoose');
+
+const username = "Udacity";
+const password = "Hossam12345";
+const cluster = "ecommerce";
+const dbname = "Ecommerce";
+mongoose.connect(
+    `mongodb+srv://${username}:${password}@${cluster}.vmn8id9.mongodb.net/?retryWrites=true&w=majority`, 
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
+
+
