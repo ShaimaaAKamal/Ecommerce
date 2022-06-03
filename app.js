a5ccf0d3ffe0a05ab57b377b7f4dfc7b4eeebaa0
Shaimaa Kamal <Shaimaakamal039@gmail.com>
2022-06-03 20:36:14 +0200

    Start Server App

---
+++ app.js
@@ -0,0 +1,7 @@
+const express=require('express');
+const app= express();
+
+
+app.listen(3000,()=>{
+    console.log('Server is running on https://localhost:3000 to stop the server please press ctrl + c');
+})
