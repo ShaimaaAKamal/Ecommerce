a62f6d061e9765a0980379ca3b6a43794f16e27d
Shaimaa Kamal <Shaimaakamal039@gmail.com>
2022-06-07 00:11:10 +0200

    implement get  products api

--- controller/product/productController.js
+++ controller/product/productController.js
@@ -15,5 +15,10 @@ const addProductController=async (req,res) =>{
             return displayError(res,500,false,"Something went Wrong",err)}} 
 }
 
+const getProductController=async (req,res) => {
+    const products=await Product.find();
+    if(products)   return displayData(res,200,true,"Products has been successfully Retreived",{products});
+    else   return displayCustomError(res,400,false,"There are no products exist")
+}
 
-module.exports={addProductController}
+module.exports={addProductController,getProductController}
--- routes/product/product.js
+++ routes/product/product.js
@@ -2,11 +2,13 @@ const express=require("express");
 const router=express.Router();
 const passport_authenticate_jwt=require('../../middleware/authenticate');
 const isAdmin=require('../../middleware/isAdmin')
-const addProductController=require("../../controller/product/productController").addProductController
+const {addProductController,getProductController}=require("../../controller/product/productController")
 
 
 
 router.post('/',passport_authenticate_jwt((req,res,next)=>{next()}),isAdmin,addProductController)
+router.get('/',getProductController)
+
 
 
 
