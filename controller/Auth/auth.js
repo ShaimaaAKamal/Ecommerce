08c7978f3b20a3f0a3dde12783990a685cc33218
Shaimaa Kamal <Shaimaakamal039@gmail.com>
2022-06-06 18:19:14 +0200

    rearrange router file and implents the controller

---
+++ controller/Auth/auth.js
@@ -0,0 +1,55 @@
+const generateJWT =require("../../helpers/jwtGeneration");
+const {displayCustomError,displayError,displayData}=require("../../helpers/display");
+const User=require("../../db/models/userModel");
+const passport_authenticate_jwt=require('../../middleware/authenticate');
+
+
+const loginController = async (req,res)=>{
+    if(Object.keys(req.body).length === 0){
+       return displayCustomError(res,400,false,"there are a missing fields")} 
+
+    else{
+        const user =await User.findOne({email:req.body.email})
+        if(!user){
+            return displayCustomError(res,401,false,"this user is not exist")
+        }
+        else{
+             user.comparePassword(req.body.password,(err,isMatch)=>{
+                if(err)
+                {
+                    return displayError(res,500,false,"Something went Wrong",err)  
+                }
+                else if (!isMatch) {
+                    return displayCustomError(res,401,false,"Password doesn't match");
+                }
+                else{
+                     const token=generateJWT(user).token;
+                     return displayData(res,200,true,"User has been successfully login",{user,token}); }})}}
+}
+
+
+
+const registerController= async (req,res)=>{
+
+    if(Object.keys(req.body).length === 0){
+        return displayCustomError(res,400,false,"there are a missing fields");
+    } 
+    else{
+        try{
+          let user = await User.create(req.body)
+          return displayData(res,200,true,"User has been successfully added",{user});
+        }
+        catch(err){
+            return displayError(res,500,false,"Something went Wrong",err)}}  
+}
+
+
+const profileController = passport_authenticate_jwt((req,res,next)=>{
+    const token=req.headers.authorization;
+    const user=req.user;
+    return displayData(res,200,true,"User has been successfully Retreived",{user,token});
+
+})
+
+
+module.exports={loginController,registerController,profileController}
--- routes/Auth/auth.js
+++ routes/Auth/auth.js
@@ -1,56 +1,11 @@
 const express=require('express');
-const passport=require('passport');
 const router=express.Router();
-const generateJWT =require("../../helpers/jwtGeneration");
-const {displayCustomError,displayError,displayData}=require("../../helpers/display");
-const User=require("../../db/models/userModel");
-const passport_authenticate_jwt=require('../../middleware/authenticate');
+const {loginController,registerController,profileController}=require("../../controller/Auth/auth")
 
+router.post('/login',loginController )
 
+router.post('/register',registerController)
 
-
-router.post('/login', async (req,res)=>{
-    if(Object.keys(req.body).length === 0){
-       return displayCustomError(res,400,false,"there are a missing fields")} 
-
-    else{
-        const user =await User.findOne({email:req.body.email})
-        if(!user){
-            return displayCustomError(res,401,false,"this user is not exist")
-        }
-        else{
-             user.comparePassword(req.body.password,(err,isMatch)=>{
-                if(err)
-                {
-                    return displayError(res,500,false,"Something went Wrong",err)  
-                }
-                else if (!isMatch) {
-                    return displayCustomError(res,401,false,"Password doesn't match");
-                }
-                else{
-                     const token=generateJWT(user).token;
-                     return displayData(res,200,true,"User has been successfully login",{user,token}); }})}}
-})
-
-router.post('/register',async (req,res)=>{
-
-    if(Object.keys(req.body).length === 0){
-        return displayCustomError(res,400,false,"there are a missing fields");
-    } 
-    else{
-        try{
-          let user = await User.create(req.body)
-          return displayData(res,200,true,"User has been successfully added",{user});
-        }
-        catch(err){
-            return displayError(res,500,false,"Something went Wrong",err)}}  
-})
-
-router.get('/profile' , passport_authenticate_jwt((req,res)=>{
-    const token=req.headers.authorization;
-    const user=req.user;
-    return displayData(res,200,true,"User has been successfully Retreived",{user,token});
-
-}))
+router.get('/profile' , profileController)
 
 module.exports=router;
