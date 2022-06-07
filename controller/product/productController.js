5d3ff019e2868bc67869ad0ed93f4e43c70d20ef
Shaimaa Kamal <Shaimaakamal039@gmail.com>
2022-06-07 22:56:00 +0200

    implement update api and update get all users api

--- controller/product/productController.js
+++ controller/product/productController.js
@@ -16,16 +16,22 @@ const addProductController=async (req,res) =>{
 }
 
 const getProductsController=async (req,res) => {
-    const products=await Product.find();
-    if(products)   return displayData(res,200,true,"Products has been successfully Retreived",{products});
-    else   return displayCustomError(res,400,false,"There are no products exist")
-}
+    try{let msg;
+        const products=await Product.find();
+        if(products.length !=0)     msg="Products has been successfully Retreived";
+        else  msg ="There are no products exist"; 
+        return displayData(res,200,true,msg,{products});
+    }
+    catch(err){
+        return displayError(res,500,false,"Something went Wrong",err);
+    }}
+
 
 const getSingleProductController= async (req,res) =>{
     const id=req.params.productId;
     try{
         const product=await Product.find({_id:id});
-        if(product.length != 0) return displayData(res,200,true,"Product has been successfully added",{product});
+        if(product.length != 0) return displayData(res,200,true,"Product has been successfully retrieved",{product});
         else return displayCustomError(res,404,false,"There are no products exist")
     }catch(err){
         return displayError(res,500,false,"Something went Wrong",err)
@@ -36,12 +42,26 @@ const deleteSingleProductController=async (req,res)=>{
     try{
         const product=await Product.findOneAndDelete({_id:id});
         if(product) return displayData(res,200,true,"Product has been successfully deleted",{product});
-        else return displayCustomError(res,404,false,"There are no products exist")
+        else return displayCustomError(res,404,false,"There are no such a product exist")
     }catch(err){
         return displayError(res,500,false,"Something went Wrong",err)
     }} 
 
+const updateSingleProductController=async (req,res) => {
+    if(Object.keys(req.body).length === 0){
+        return displayCustomError(res,400,false,"there are a missing fields");
+    } 
+    else{
+        try{
+        const id=req.params.productId;
+        const product=await Product.findOneAndUpdate({_id:id},req.body,{new:true});
+        console.log(product)
+        if(product.length != 0) return displayData(res,200,true,"Product has been successfully updated",{product});
+        else return displayCustomError(res,404,false,"There is no such a product exists")
+    }catch(err){
+        return displayError(res,500,false,"Something went Wrong",err)
+    }
+}}
 
 
-
-module.exports={addProductController,getProductsController,getSingleProductController,deleteSingleProductController}
+module.exports={addProductController,getProductsController,getSingleProductController,deleteSingleProductController,updateSingleProductController}
--- routes/product/product.js
+++ routes/product/product.js
@@ -2,7 +2,7 @@ const express=require("express");
 const router=express.Router();
 const passport_authenticate_jwt=require('../../middleware/authenticate');
 const isAdmin=require('../../middleware/isAdmin')
-const {addProductController,getProductsController,getSingleProductController,deleteSingleProductController}=require("../../controller/product/productController")
+const {addProductController,getProductsController,getSingleProductController,deleteSingleProductController,updateSingleProductController}=require("../../controller/product/productController")
 
 
 
@@ -10,6 +10,7 @@ router.post('/',passport_authenticate_jwt((req,res,next)=>{next()}),isAdmin,addP
 router.get('/',getProductsController);
 router.get('/:productId',getSingleProductController);
 router.delete('/:productId',passport_authenticate_jwt((req,res,next)=>{next()}),isAdmin,deleteSingleProductController);
+router.put('/:productId',passport_authenticate_jwt((req,res,next)=>{next()}),isAdmin,updateSingleProductController);
 
 
 
