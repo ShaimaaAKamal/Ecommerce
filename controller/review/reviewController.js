696212151fe403dbe9d17d11d06cb180b15f41a2
Shaimaa Kamal <Shaimaakamal039@gmail.com>
2022-06-14 11:37:17 +0200

    update add review api

--- controller/review/reviewController.js
+++ controller/review/reviewController.js
@@ -1,14 +1,30 @@
 const Review=require("../../db/models/reviewModel");
+const User=require("../../db/models/userModel");
+const Product=require("../../db/models/productModel");
+
+
 const {displayCustomError,displayError,displayData}=require("../../helpers/display");
 
 
 const addReviewController=async (req,res) =>{
         try{  req.body.user=req.user._id;
-          let review = await Review.create(req.body)
-          return displayData(res,200,true,"Review has been successfully added",{review});
+            if(Object.keys(req.body).length === 0){
+                return displayCustomError(res,400,false,"You must enter review field");}
+            else if (req.body.rate || req.body.review){
+                let review = await Review.create(req.body);
+                if(review) {
+                    let user=await User.findById(req.user._id).exec();
+                    user.reviews.push(review);
+                    await User.updateOne({_id:req.user._id},{reviews:user.reviews});
+                    let product=await Product.findById(req.body.product).exec();
+                    product.reviews.push(review);
+                    await Product.updateOne({_id:req.body.product},{reviews:product.reviews});
+                }
+          return displayData(res,200,true,"Review has been successfully added",{review}); 
+            }
+            else  return displayCustomError(res,400,false,"You must enter rate  field");
         }
-        catch(err){
-            return displayError(res,500,false,"Something went Wrong",err)}
+        catch(err){ return displayError(res,500,false,"Something went Wrong",err)}
 }
 
 const getReviewsController=async (req,res) => {
