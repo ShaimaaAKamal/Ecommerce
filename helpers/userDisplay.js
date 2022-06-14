e8af23dbf174262092f7d2cb5a534b712536f6cd
Shaimaa Kamal <Shaimaakamal039@gmail.com>
2022-06-14 23:31:35 +0200

    update user display helper function

--- helpers/userDisplay.js
+++ helpers/userDisplay.js
@@ -1,7 +1,7 @@
 const returnUsersDetails=(user)=>{
     let userDetails={};
     Object.keys(user._doc).forEach((key)=>{
-    if(key == "_id" || key == "status" || key == "orders" || key == "reviews"  || key == "username" ) userDetails[key]=user[key];
+    if(key == "_id" || key == "status" || key == "orders" || key == "reviews"  || key == "username" || key == "created" ) userDetails[key]=user[key];
      else return;
    })
    return userDetails;
