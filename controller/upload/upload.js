013cf066af6acc686948a1392ed6a748c6e2672b
Shaimaa Kamal <Shaimaakamal039@gmail.com>
2022-06-15 23:45:38 +0200

    update upload image controller

--- controller/upload/upload.js
+++ controller/upload/upload.js
@@ -1,6 +1,7 @@
 
 const {displayCustomError,displayData}=require("../../helpers/display");
 const path=require('path');
+const Resize= require("../../helpers/resize");
 
 
 const uploadImage=async(req,res)=>{
@@ -8,6 +9,7 @@ const uploadImage=async(req,res)=>{
     // const imgUrl = `https://shopifyall.herokuapp.com/images/${req.file.filename}`;
     // return displayData(res,200,true,"Image has been successfullu uploaded",{imgUrl});
     const imagePath = path.resolve(__dirname, '/public/images');
+    const fileUpload = new Resize(imagePath);
     console.log(imagePath);
     if (!req.file) {
         return displayCustomError(res,400,false,"Please provide an Image")
