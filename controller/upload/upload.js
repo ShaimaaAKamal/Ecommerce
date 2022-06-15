b24ea071ef17959abdaef8043ffab5932f1d9552
Shaimaa Kamal <Shaimaakamal039@gmail.com>
2022-06-15 23:10:20 +0200

    update upload image controller

--- controller/upload/upload.js
+++ controller/upload/upload.js
@@ -7,7 +7,7 @@ const uploadImage=async(req,res)=>{
     // if (req.file === undefined)  r
     // const imgUrl = `https://shopifyall.herokuapp.com/images/${req.file.filename}`;
     // return displayData(res,200,true,"Image has been successfullu uploaded",{imgUrl});
-    const imagePath = path.join(__dirname, '/public/images');
+    const imagePath = path.resolve(__dirname, '/public/images');
     console.log(imagePath);
     if (!req.file) {
         return displayCustomError(res,400,false,"Please provide an Image")
