716c2966996907a3e44772ada6f8913cfbd49e74
Shaimaa Kamal <Shaimaakamal039@gmail.com>
2022-06-16 14:17:38 +0200

    update upload image route

--- controller/upload/upload.js
+++ controller/upload/upload.js
@@ -9,6 +9,7 @@ const S3_BUCKET = process.env.S3_BUCKET;
 
 const uploadImage=async(req,res)=>{
     const s3=new aws.S3();
+
     const upload =async (image)=>{
         let extention=(image.name.split("."))[1];
         let result=  await s3.upload({
@@ -16,16 +17,15 @@ const uploadImage=async(req,res)=>{
            Key: `Brands/${Date.now()}-shopifyall.${extention}`,
            Body:image.data ,
          }).promise();
-         return result
-    }
+         return result}
 
     if (req.files.length == 0 ) {
         return displayCustomError(res,400,false,"Please provide  Images to be uploaded.")
      }
       
     const uploadedImageFun= async (req)=>{
-        if (req.files.images.length > 1){ for(let image of req.files.images) {let out = upload(image); console.log(out)}}
-        else {let out = upload(req.files.images); console.log(out)}
+        if (req.files.images.length > 1){ for(let image of req.files.images) {let out = await upload(image); console.log(out)}}
+        else {let out = await upload(req.files.images); console.log(out)}
         console.log('done');
         }
     await uploadedImageFun(req);
--- routes/upload/upload.js
+++ routes/upload/upload.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const passport_authenticate_jwt=require('../../middleware/authenticate');
+const isAdmin=require('../../middleware/isAdmin')
+
 const uploadImage=require("../../controller/upload/upload");
 
-router.post("/upload", uploadImage );
+router.post("/upload",passport_authenticate_jwt((req,res,next)=>{next()}),isAdmin,uploadImage );
 
 module.exports = router;
