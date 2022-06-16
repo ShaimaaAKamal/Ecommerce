062b3fcfcf542c0e5bb61ff1a8c0dd1160a815e9
Shaimaa Kamal <Shaimaakamal039@gmail.com>
2022-06-16 12:20:49 +0200

    update upload image controller

--- controller/upload/upload.js
+++ controller/upload/upload.js
@@ -7,6 +7,8 @@ const S3_BUCKET = process.env.S3_BUCKET;
 
 const uploadImage=async(req,res)=>{
     const s3=new aws.S3();
+    console.log(req.body)
+    console.log(req.file)
     console.log(req.files);
 
     const uploadedImage = await s3.upload({
