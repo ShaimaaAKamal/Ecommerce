f611e09751212acb1474580b96c6ff6d2dabbf8e
Shaimaa Kamal <Shaimaakamal039@gmail.com>
2022-06-16 14:34:38 +0200

    update upload image controller

--- controller/upload/upload.js
+++ controller/upload/upload.js
@@ -1,6 +1,7 @@
 
 const {displayCustomError,displayData}=require("../../helpers/display");
 const aws = require('aws-sdk');
+const { request } = require("express");
 const S3_BUCKET = process.env.S3_BUCKET;
 
 
@@ -8,11 +9,11 @@ const S3_BUCKET = process.env.S3_BUCKET;
 const uploadImage=async(req,res)=>{
     const s3=new aws.S3();
 
-    const upload =async (image)=>{
+    const upload =async (image,imageFolder)=>{
         let extention=(image.name.split("."))[1];
         let result=  await s3.upload({
            Bucket: 'shopifyallimages',
-           Key: `Brands/${Date.now()}-shopifyall.${extention}`,
+           Key: `${imageFolder}/${Date.now()}-shopifyall.${extention}`,
            Body:image.data ,
          }).promise();
          return result}
@@ -20,14 +21,18 @@ const uploadImage=async(req,res)=>{
     if (req.files.length == 0 ) {
         return displayCustomError(res,400,false,"Please provide  Images to be uploaded.")
      }
-      
-    const uploadedImageFun= async (req)=>{
-        let Images=[];
-        if (req.files.images.length > 1){ for(let image of req.files.images) {let out = await upload(image); Images.push(out);}}
-        else {let out = await upload(req.files.images); Images.push(out)}
-        return displayData(res,200,true,"Image has been successfully uploaded",{Images});
-        }
-    await uploadedImageFun(req);
+     else if (req.body.length === 0 || !req.body.imageFolder  || !req.body.folderImageId){
+        return displayCustomError(res,400,false,"You Select a Folder Id")
+     }
+     else{
+        const uploadedImageFun= async (req)=>{
+            let Images=[];
+            if (req.files.images.length > 1){ for(let image of req.files.images) {let out = await upload(image); Images.push(out);}}
+            else {let out = await upload(req.files.images); Images.push(out)}
+            return displayData(res,200,true,"Image has been successfully uploaded",{Images});
+            }
+        await uploadedImageFun(req);
+     }
 }
 
 module.exports=uploadImage;
