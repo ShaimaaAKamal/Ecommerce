876265e7e4e27994dcde9afb955cf46fb5d0d08d
Shaimaa Kamal <Shaimaakamal039@gmail.com>
2022-06-16 10:18:42 +0200

    update upload image controller

--- middleware/upload.js
+++ middleware/upload.js
@@ -25,7 +25,7 @@ const multer = require('multer');
 
 let storage = multer.diskStorage({   
     destination: function(req, file, cb) { 
-       cb(null, '../public/images');    
+       cb(null, '/public/images');    
     }, 
     filename: function (req, file, cb) { 
        cb(null , `${Date.now()}-shopify-${file.originalname}`);   
