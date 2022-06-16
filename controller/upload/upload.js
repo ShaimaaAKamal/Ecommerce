8c0bb705656200aa826eb55b13bb72383feee555
Shaimaa Kamal <Shaimaakamal039@gmail.com>
2022-06-16 10:16:53 +0200

    update upload image controller

--- controller/upload/upload.js
+++ controller/upload/upload.js
@@ -8,15 +8,17 @@ const uploadImage=async(req,res)=>{
     // if (req.file === undefined)  r
     // const imgUrl = `https://shopifyall.herokuapp.com/images/${req.file.filename}`;
     // return displayData(res,200,true,"Image has been successfullu uploaded",{imgUrl});
-    console.log(req.file);
     console.log(req.files);
     console.log(req.body);
+
+     if (!req.files) {
+        return displayCustomError(res,400,false,"Please provide  Images to be uploaded.")
+     }
+
     // const imagePath = path.resolve(__dirname, '/public/images');
     // const fileUpload = new Resize(imagePath);
     // console.log(imagePath);
-    // if (!req.files) {
-    //     return displayCustomError(res,400,false,"Please provide an Image")
-    //  }
+   
     // const filename = await fileUpload.save(req.files.buffer);
     // console.log(filename);
     // return displayData(res,200,true,"Image has been successfullu uploaded",{filename});
--- middleware/upload.js
+++ middleware/upload.js
@@ -21,13 +21,17 @@
 
 // module.exports = multer({ storage });
 
-
 const multer = require('multer');
 
-const upload = multer({
-  limits: {
-    fileSize: 4 * 1024 * 1024,
-  }
-});
+let storage = multer.diskStorage({   
+    destination: function(req, file, cb) { 
+       cb(null, '../public/images');    
+    }, 
+    filename: function (req, file, cb) { 
+       cb(null , `${Date.now()}-shopify-${file.originalname}`);   
+    }
+ });
+
+ let upload = multer({ storage: storage , limits : {fileSize : 3000000}});
 
 module.exports = upload
