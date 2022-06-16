4dc832fd3e94c1fbd254c98df6af08ba90ea6a3f
Shaimaa Kamal <Shaimaakamal039@gmail.com>
2022-06-16 13:52:50 +0200

    update upload image controller

--- controller/upload/upload.js
+++ controller/upload/upload.js
@@ -5,26 +5,29 @@ const {displayCustomError,displayData}=require("../../helpers/display");
 const aws = require('aws-sdk');
 const S3_BUCKET = process.env.S3_BUCKET;
 
+
+
 const uploadImage=async(req,res)=>{
     const s3=new aws.S3();
+    const upload =async ()=>{
+        let extention=(image.name.split("."))[1];
+        let result=  await s3.upload({
+           Bucket: 'shopifyallimages',
+           Key: `Brands/${Date.now()}-shopifyall.${extention}`,
+           Body:image.data ,
+         }).promise();
+         return result
+    }
 
-    if (!req.files) {
+    if (!req.files || ! req.file) {
         return displayCustomError(res,400,false,"Please provide  Images to be uploaded.")
      }
       
     const uploadedImageFun= async (req)=>{
-        for(let image of req.files.images){
-            let extention=(image.name.split("."))[1];
-            console.log(extention);
-          let result=  await s3.upload({
-                Bucket: 'shopifyallimages',
-                Key: `Brands/${Date.now()}-shopifyall.${extention}`,
-                Body:image.data ,
-              }).promise()
-
-              console.log(result);
-             
+        if (req.files){ for(let image of req.files.images) {let out = upload(); console.log(out)}}
+        else if(req.file) {let out = upload(); console.log(out)}
         }
+     
         console.log('done');
     } 
     await uploadedImageFun(req);
